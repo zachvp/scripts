c49f1651f53bc29789f7d7ba13dc1511e2b60ac6
zachvp <vegaperk@gmail.com>
2023-11-04 16:28:58 -0600

    add timing for transfer functions

--- bash/corevega-ctrl.sh
+++ bash/corevega-ctrl.sh
@@ -1,7 +1,6 @@
 #/bin/bash
 
 COREVEGA_FUNCTION=$1
-COREVEGA_MUSIC_MEDIA_SOURCE=$2
 COREVEGA_IP_ETHERNET="192.168.2.2"
 COREVEGA_RSYNC_PORT=12000
 
@@ -20,24 +19,27 @@ run_rsync_daemon()
 ### Preferred: direct ethernet+ssh+daemon transfer
 run_rsync_transfer_daemon()
 {
+    COREVEGA_MUSIC_MEDIA_SOURCE=$2
     COREVEGA_TRANSFER_DEST="rsync://zachvp@$COREVEGA_IP_ETHERNET:$COREVEGA_RSYNC_PORT/music"
 
     echo "transfer source: $COREVEGA_MUSIC_MEDIA_SOURCE"
     echo "transfer destination: $COREVEGA_TRANSFER_DEST"
-	rsync $COREVEGA_MUSIC_MEDIA_SOURCE $COREVEGA_TRANSFER_DEST \
+	time rsync $COREVEGA_MUSIC_MEDIA_SOURCE $COREVEGA_TRANSFER_DEST \
         --progress -rtvzi --exclude ".*"
 }
 
 ### Simple setup: direct ethernet+ssh transfer
 run_rsync_transfer()
 {
-	rsync $COREVEGA_MUSIC_MEDIA_SOURCE zachvp@$COREVEGA_IP_ETHERNET:/media/zachvp/SOL/transfer-rsync/rsync_default \
+    COREVEGA_MUSIC_MEDIA_SOURCE=$2
+	time rsync $COREVEGA_MUSIC_MEDIA_SOURCE zachvp@$COREVEGA_IP_ETHERNET:/media/zachvp/SOL/transfer-rsync/rsync_default \
         --progress -rtvzi --exclude ".*"
 }
 
 ## run container on host port 8000
 run_docker_media_music_stream()
 {
+    echo "running subsonic_corevega"
     docker run -dti \
         --publish 8000:4040 \
         --name="subsonic_corevega" \
