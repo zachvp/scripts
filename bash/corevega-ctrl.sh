ce8cb06e89470db70808798faab7a1641514ff59
zachvp <vegaperk@gmail.com>
2023-11-04 18:07:47 -0600

    add rsyncd qol functions

--- bash/corevega-ctrl.sh
+++ bash/corevega-ctrl.sh
@@ -3,51 +3,55 @@
 COREVEGA_FUNCTION=$1
 COREVEGA_IP_ETHERNET="192.168.2.2"
 COREVEGA_RSYNC_PORT=12000
-COREVEGA_HOME=/home/zachvp
+COREVEGA_PATH_HOME=/home/zachvp
+COREVEGA_PATH_RSYNC_PID="$COREVEGA_PATH_HOME/developer/state/rsync/rsyncd.pid"
 
-## server: run rsync daemon
-run_rsync_daemon()
+## UTIL
+cv_echo()
 {
-    COREVEGA_DAEMON_CONFIG_FILE=$COREVEGA_HOME/.config/rsync/default.conf
-	echo "function requires sudo access, continue? [y/N]"
-	read CONTINUE
-	if [ $CONTINUE == "y" ]; then
-    	sudo rsync --daemon --config $COREVEGA_DAEMON_CONFIG_FILE -v
+	PREFIX="corevega"
+	echo "$PREFIX: $1"
+}
 
-		COREVEGA_RSYNC_PID=`cat $COREVEGA_HOME/developer/state/rsync/rsyncd.pid`
-		echo "daemon running, PID: $COREVEGA_RSYNC_PID"
-		ps aux | grep 'rsync -v --daemon'
+## SERVER
+ps_rsync_daemon()
+{
+	if [ -e "$COREVEGA_PATH_RSYNC_PID" ]; then
+		cv_echo "daemon PID: $(cat $COREVEGA_PATH_RSYNC_PID)"
+		ps aux | grep $(cat $COREVEGA_PATH_RSYNC_PID) | grep -v 'grep'
 	else
-		echo "user declined to continue"
+		cv_echo "daemon is not running"
 	fi
 }
 
-## client: run rsync
-
-### Preferred: direct ethernet+ssh+daemon transfer
-run_rsync_transfer_daemon()
+## run rsync daemon
+start_rsync_daemon()
 {
-    COREVEGA_MUSIC_MEDIA_SOURCE=$2
-    COREVEGA_TRANSFER_DEST="rsync://zachvp@$COREVEGA_IP_ETHERNET:$COREVEGA_RSYNC_PORT/music"
-
-    echo "transfer source: $COREVEGA_MUSIC_MEDIA_SOURCE"
-    echo "transfer destination: $COREVEGA_TRANSFER_DEST"
-	time rsync $COREVEGA_MUSIC_MEDIA_SOURCE $COREVEGA_TRANSFER_DEST \
-        --progress -rtvzi --exclude ".*"
+    COREVEGA_DAEMON_CONFIG_FILE=$COREVEGA_PATH_HOME/.config/rsync/default.conf
+	cv_echo "function requires sudo access, continue? [y/N]"
+	read CONTINUE
+	if [ $CONTINUE == "y" ]; then
+    	sudo rsync --daemon --config $COREVEGA_DAEMON_CONFIG_FILE -v
+		ps_rsync_daemon
+	else
+		cv_echo "user declined to continue"
+	fi
 }
 
-### Simple setup: direct ethernet+ssh transfer
-run_rsync_transfer()
+stop_rsync_daemon()
 {
-    COREVEGA_MUSIC_MEDIA_SOURCE=$2
-	time rsync $COREVEGA_MUSIC_MEDIA_SOURCE zachvp@$COREVEGA_IP_ETHERNET:/media/zachvp/SOL/transfer-rsync/rsync_default \
-        --progress -rtvzi --exclude ".*"
+	if [ -e "$COREVEGA_PATH_RSYNC_PID" ]; then
+		sudo kill "$(cat $COREVEGA_PATH_RSYNC_PID)"
+	else
+		cv_echo "rsync daemon not running, nothing to do"
+	fi
 }
+
 
 ## run container on host port 8000
 run_docker_media_music_stream()
 {
-    echo "running subsonic_corevega"
+    cv_echo "running subsonic_corevega"
     docker run -dti \
         --publish 8000:4040 \
         --name="subsonic_corevega" \
@@ -59,7 +63,32 @@ run_docker_media_music_stream()
         hydria/subsonic:latest
 }
 
+# ===
+
+## CLIENT 
+### run rsync
+#### Preferred: direct ethernet+ssh+daemon transfer
+run_rsync_transfer_daemon()
+{
+    COREVEGA_MUSIC_MEDIA_SOURCE=$2
+    COREVEGA_TRANSFER_DEST="rsync://zachvp@$COREVEGA_IP_ETHERNET:$COREVEGA_RSYNC_PORT/music"
+
+    echo "transfer source: $COREVEGA_MUSIC_MEDIA_SOURCE"
+    echo "transfer destination: $COREVEGA_TRANSFER_DEST"
+	time rsync $COREVEGA_MUSIC_MEDIA_SOURCE $COREVEGA_TRANSFER_DEST \
+        --progress -rtvzi --exclude ".*"
+}
+
+#### Simple setup: direct ethernet+ssh transfer
+run_rsync_transfer()
+{
+    COREVEGA_MUSIC_MEDIA_SOURCE=$2
+	time rsync $COREVEGA_MUSIC_MEDIA_SOURCE zachvp@$COREVEGA_IP_ETHERNET:/media/zachvp/SOL/transfer-rsync/rsync_default \
+        --progress -rtvzi --exclude ".*"
+}
+
+# MAIN
 # run the function argument
-echo "running: $COREVEGA_FUNCTION"
+cv_echo "$COREVEGA_FUNCTION"
 $COREVEGA_FUNCTION
 
