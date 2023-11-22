97597ff2b322e425cb25a3cece3d70eea48a9bbf
zachvp <vegaperk@gmail.com>
2023-11-22 15:24:34 -0700

    some reorg and cleanup

--- djmgmt/restore_collection_metadata.py
+++ djmgmt/restore_collection_metadata.py
@@ -1,31 +1,29 @@
+'''
+This script is rekordbox XML specific.
+
+Given a collection with innaccurate data, this script will construct a restored XML
+collection based on another given collection with accurate information.
+
+The following Track attributes will be corrected:
+	- 'DateAdded'
+'''
+
 import sys
 import xml.etree.ElementTree as ET
 
-'''
-Data: source collection (S)
-Data: modify collection (M)
-Data: output collection (O)
+# HELPERS
+def generate_id(node: ET.Element) -> str:
+	assert node.tag == 'TRACK', f"unexpected element tag: {node.tag}"
 
-O = M.Copy()
+	# the id is the concenation of 2 shards
+	# shard_0 sources from track name
+	# shard_1 sources from track artist, total time, and BPM
+	len_0 = min(8, len(node.attrib[ATTR_NAME]))
+	len_1 = min(8, len(node.attrib[ATTR_ARTIST]))
+	shard_0 = f"{node.attrib[ATTR_NAME][:len_0]}{node.attrib[ATTR_NAME][-len_0:]}"
+	shard_1 = f"{node.attrib[ATTR_ARTIST][:len_1]}{node.attrib[ATTR_TOTAL_TIME]}{node.attrib[ATTR_AVG_BPM]}"
 
-Reads S
-	Collects "Date Added" values (D)
-	Associates "TotalTime", "BPM" values (V) with D
-	Store (D, V) in list (L)
-
-For each item in L
-	find v in M
-		o.dateAdded = item.dateAdded
-
-
-'''
-
-# -- HELPERS
-def generate_id(node):
-	# name shard is first 8 chars and last 8 chars concatenated
-	# shard_name_len = node.attrib
-	shard_name = f"{node.attrib[ATTR_NAME][:8]}{node.attrib[ATTR_NAME][-8:]}"
-	id_str = f"{shard_name}{node.attrib[ATTR_ARTIST][:4]}{node.attrib[ATTR_TOTAL_TIME]}{node.attrib[ATTR_AVG_BPM]}"
+	id_str = f"{shard_0}{shard_1}"
 	# print(f"generated id: {id_str}")
 	return id_str
 
@@ -38,13 +36,13 @@ ATTR_TOTAL_TIME = 'TotalTime'
 ATTR_AVG_BPM = 'AverageBpm'
 ATTR_DATE_ADDED = 'DateAdded'
 
-# -- MAIN
+# MAIN
 
 # user input validation
 if len(sys.argv) != 4:
 	arg_format =\
 	'''
-	1: path to collection you want to correct
+	1: path to incorrectly dated collection
 	2: path to the source collection with the correct dates
 	3: path to the output location of the corrected collection'''
 
@@ -57,7 +55,7 @@ path_collection_corrected = sys.argv[2]
 path_collection_output = sys.argv[3]
 
 # data: parsed user input
-tree_output = ET.parse(path_collection_current)
+tree_incorrect = ET.parse(path_collection_current)
 tree_corrected = ET.parse(path_collection_corrected)
 
 # data: script state, mutable
@@ -73,7 +71,7 @@ for node in tree_corrected.getroot().find(XPATH_COLLECTION):
 	corrected_dates[track_id] = node.attrib[ATTR_DATE_ADDED]
 
 # find all tracks to correct in the current collection
-for node in tree_output.getroot().find(XPATH_COLLECTION):
+for node in tree_incorrect.getroot().find(XPATH_COLLECTION):
 	track_id = generate_id(node)
 	if track_id in corrected_dates:
 		if node.attrib[ATTR_DATE_ADDED] != corrected_dates[track_id]:
@@ -81,5 +79,5 @@ for node in tree_output.getroot().find(XPATH_COLLECTION):
 			node.attrib[ATTR_DATE_ADDED] = corrected_dates[track_id]
 
 # write the corrected collection to the specified file
-tree_output.write(file_or_filename=path_collection_output, encoding='UTF-8', xml_declaration=True)
+tree_incorrect.write(file_or_filename=path_collection_output, encoding='UTF-8', xml_declaration=True)
 
