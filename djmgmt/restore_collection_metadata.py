d82144fadd9be9e1f7414195d86fd663b566a2a0
zachvp <vegaperk@gmail.com>
2023-11-22 17:59:03 -0700

    address lint feedback, re-org into main script() function

--- djmgmt/restore_collection_metadata.py
+++ djmgmt/restore_collection_metadata.py
@@ -5,7 +5,7 @@ Given a collection with innaccurate data, this script will construct a restored 
 collection based on another given collection with accurate information.
 
 The following Track attributes will be corrected:
-	- 'DateAdded'
+    - 'DateAdded'
 '''
 
 import sys
@@ -13,19 +13,20 @@ import xml.etree.ElementTree as ET
 
 # HELPERS
 def generate_id(node: ET.Element) -> str:
-	assert node.tag == 'TRACK', f"unexpected element tag: {node.tag}"
+    assert node.tag == 'TRACK', f"unexpected element tag: {node.tag}"
 
-	# the id is the concenation of 2 shards
-	# shard_0 sources from track name
-	# shard_1 sources from track artist, total time, and BPM
-	len_0 = min(8, len(node.attrib[ATTR_NAME]))
-	len_1 = min(8, len(node.attrib[ATTR_ARTIST]))
-	shard_0 = f"{node.attrib[ATTR_NAME][:len_0]}{node.attrib[ATTR_NAME][-len_0:]}"
-	shard_1 = f"{node.attrib[ATTR_ARTIST][:len_1]}{node.attrib[ATTR_TOTAL_TIME]}{node.attrib[ATTR_AVG_BPM]}"
+    # the id is the concenation of 2 shards
+    # shard_0 sources from track name
+    # shard_1 sources from track artist, total time, and BPM
+    len_0 = min(8, len(node.attrib[ATTR_NAME]))
+    len_1 = min(8, len(node.attrib[ATTR_ARTIST]))
+    shard_0 = f"{node.attrib[ATTR_NAME][:len_0]}{node.attrib[ATTR_NAME][-len_0:]}"
+    shard_1 = f"{node.attrib[ATTR_ARTIST][:len_1]}\
+        {node.attrib[ATTR_TOTAL_TIME]}{node.attrib[ATTR_AVG_BPM]}"
 
-	id_str = f"{shard_0}{shard_1}"
-	# print(f"generated id: {id_str}")
-	return id_str
+    id_str = f"{shard_0}{shard_1}"
+    # print(f"generated id: {id_str}")
+    return id_str
 
 # CONSTANTS
 XPATH_COLLECTION = './/COLLECTION'
@@ -40,44 +41,55 @@ ATTR_DATE_ADDED = 'DateAdded'
 
 # user input validation
 if len(sys.argv) != 4:
-	arg_format =\
-	'''
-	1: path to incorrectly dated collection
-	2: path to the source collection with the correct dates
-	3: path to the output location of the corrected collection'''
+    ARG_FORMAT =\
+    '''
+    1: path to incorrectly dated collection
+    2: path to the source collection with the correct dates
+    3: path to the output location of the corrected collection'''
 
-	print(f"error: incorrect usage, provide exactly 4 arguments with format:{arg_format}\nexiting...")
-	exit()
+    print(f"error: incorrect usage, provide exactly 4 arguments with format:{ARG_FORMAT}\n\
+        exiting...")
+    sys.exit()
 
-# data: user input
-path_collection_current = sys.argv[1]
-path_collection_corrected = sys.argv[2]
-path_collection_output = sys.argv[3]
+def script(
+    path_collection_current: str,
+    path_collection_corrected : str,
+    path_collection_output: str):
 
-# data: parsed user input
-tree_incorrect = ET.parse(path_collection_current)
-tree_corrected = ET.parse(path_collection_corrected)
+    # data: parsed user input
+    tree_incorrect = ET.parse(path_collection_current)
+    tree_corrected = ET.parse(path_collection_corrected)
 
-# data: script state, mutable
-corrected_dates = {}
+    # data: script state, mutable
+    corrected_dates = {}
 
-# associate each track with a unifying 'id' and collect all corrected dates
-for node in tree_corrected.getroot().find(XPATH_COLLECTION):
-	track_id = generate_id(node)
+    # associate each track with a unifying 'id' and collect all corrected dates
+    for node in tree_corrected.getroot().find(XPATH_COLLECTION):
+        track_id = generate_id(node)
 
-	if track_id in corrected_dates:
-		print(f"WARN: generated duplicate ID for track: {node.attrib[ATTR_NAME]}; existing: {corrected_dates[track_id]}. New entry will overwrite existing entry.")
+        if track_id in corrected_dates:
+            print(f'''\
+                WARN: generated duplicate ID for track: {node.attrib[ATTR_NAME]};
+                existing: {corrected_dates[track_id]}.
+                New entry will overwrite existing entry.''')
 
-	corrected_dates[track_id] = node.attrib[ATTR_DATE_ADDED]
+        corrected_dates[track_id] = node.attrib[ATTR_DATE_ADDED]
 
-# find all tracks to correct in the current collection
-for node in tree_incorrect.getroot().find(XPATH_COLLECTION):
-	track_id = generate_id(node)
-	if track_id in corrected_dates:
-		if node.attrib[ATTR_DATE_ADDED] != corrected_dates[track_id]:
-			print(f"correcting track {node.attrib[ATTR_ARTIST]}-{node.attrib[ATTR_NAME]} to use date {corrected_dates[track_id]}")
-			node.attrib[ATTR_DATE_ADDED] = corrected_dates[track_id]
+    # find all tracks to correct in the current collection
+    for node in tree_incorrect.getroot().find(XPATH_COLLECTION):
+        track_id = generate_id(node)
+        if track_id in corrected_dates:
+            if node.attrib[ATTR_DATE_ADDED] != corrected_dates[track_id]:
+                print(f"correcting track {node.attrib[ATTR_ARTIST]}-{node.attrib[ATTR_NAME]} to use\
+                    date {corrected_dates[track_id]}")
+                node.attrib[ATTR_DATE_ADDED] = corrected_dates[track_id]
 
-# write the corrected collection to the specified file
-tree_incorrect.write(file_or_filename=path_collection_output, encoding='UTF-8', xml_declaration=True)
+    # write the corrected collection to the specified file
+    tree_incorrect.write(
+        file_or_filename=path_collection_output,
+        encoding='UTF-8',
+        xml_declaration=True)
 
+# MAIN
+if __name__ == '__main__':
+    script(sys.argv[1], sys.argv[2], sys.argv[3])
