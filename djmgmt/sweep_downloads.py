611d26ce0f70e1f1a1ea506ab5de445fc6cc81f9
zachvp <vegaperk@gmail.com>
2023-12-04 12:42:49 -0700

    support multiple script function types; basic flatten dir impl

--- djmgmt/sweep_downloads.py
+++ djmgmt/sweep_downloads.py
@@ -14,7 +14,9 @@ import shutil
 import zipfile
 
 # CONSTANTS
-SOUND_TYPES = {'.mp3', '.wav', '.aif', '.aiff', 'flac'}
+FUNCTION_SWEEP = 'sweep'
+FUNCTION_FLATTEN = 'flatten'
+FUNCTIONS = {FUNCTION_FLATTEN, FUNCTION_SWEEP}
 
 # todo: move this to main procedure and flesh out more fully
 def flatten_zip(zip_path: str, extract_path: str) -> None:
@@ -28,17 +30,33 @@ def flatten_zip(zip_path: str, extract_path: str) -> None:
             shutil.move(os.path.join(working_dir, name), extract_path)
     shutil.rmtree(unzipped_path)
 
+def flatten_hierarchy(top_path: str, output_path: str) -> None:
+    for working_dir, directories, filenames in os.walk(top_path):
+        prune(working_dir, directories, filenames)
+
+        for name in filenames:
+            input_path = os.path.join(working_dir, name)
+            if os.path.splitext(name)[1] == '.zip':
+                # flatten_zip(os.path.join(working_dir, name), output_path)
+                print(f"flatten zip: {input_path, output_path}")
+            elif not os.path.exists(os.path.join(output_path, name)):
+                print(f"move file {input_path} to {os.path.join(output_path, name)}")
+            else:
+                print(f"info: skip: {input_path}")
+
+def prune(working_dir: str, directories: list[str], filenames: list[str]) -> None:
+    for index, directory in enumerate(directories):
+        if directory.startswith('.') or '.app' in directory:
+            print(f"info: skip: hidden directory or '.app' archive '{os.path.join(working_dir, directory)}'")
+            del directories[index]
+    for index, name in enumerate(filenames):
+        if name.startswith('.'):
+            print(f"info: skip: hidden file '{name}'")
+            del filenames[index]
+
 def sweep(args: argparse.Namespace) -> None:
     for working_dir, directories, filenames in os.walk(args.input):
-        # prune hidden directories
-        for index, directory in enumerate(directories):
-            if directory.startswith('.') or '.app' in directory:
-                print(f"info: skip: hidden directory or '.app' archive '{os.path.join(working_dir, directory)}'")
-                del directories[index]
-        for index, filename in enumerate(filenames):
-            if filename.startswith('.'):
-                print(f"info: skip: hidden file '{filename}'")
-                del filenames[index]
+        prune(working_dir, directories, filenames)
 
         for name in filenames:
             input_path = os.path.join(working_dir, name)
@@ -57,12 +75,17 @@ def sweep(args: argparse.Namespace) -> None:
 
 def parse_args() -> argparse.Namespace:
     parser = argparse.ArgumentParser()
+    parser.add_argument('function', type=str, help=f"The script function to run. One of '{FUNCTIONS}'")
     parser.add_argument('input', type=str, help='The input directory to sweep.')
     parser.add_argument('output', type=str, help='The output directory to place the swept tracks.')
     parser.add_argument('--zip-filter', type=str )
     parser.add_argument('--interactive', '-i', action='store_true')
 
     args = parser.parse_args()
+
+    if args.function not in FUNCTIONS:
+        parser.error(f"invalid function '{args.function}'")
+
     args.input = os.path.normpath(args.input)
     args.output = os.path.normpath(args.output)
 
@@ -71,5 +94,10 @@ def parse_args() -> argparse.Namespace:
 if __name__ == '__main__':
     script_args = parse_args()
 
-    flatten_zip(script_args.input, script_args.output)
-    # sweep(script_args)
+    if script_args.function == 'sweep':
+        print('fake sweep')
+        # sweep(script_args)
+    elif script_args.function == 'flatten':
+        print('fake flatten')
+        flatten_hierarchy(script_args.input, script_args.output)
+        # flatten_zip(script_args.input, script_args.output)
