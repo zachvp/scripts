df572b018d3ee3e4f75dec131b588a6676593d8f
zachvp <vegaperk@gmail.com>
2023-12-19 13:44:49 -0700

    remove old unused function

--- djmgmt/populate_media_server.py
+++ djmgmt/populate_media_server.py
@@ -12,24 +12,21 @@ specs
         output: D directory path
 
 process
-    OLDEST: identify oldest year/month directory present in input
+    OLDEST: identify oldest year/month directory tree present in input path
     if OLDEST exists in destination
         skip
-    copy OLDEST to output
+    copy OLDEST to output path
 '''
 
 import argparse
 import os
 import shutil
-import sys
-import pdb
 
 def parse_args() -> argparse.Namespace:
     parser = argparse.ArgumentParser()
     parser.add_argument('input', type=str, help="The top level directory to search.\
         It's expected to be structured in a year/month/audio.file format.")
     parser.add_argument('output', type=str, help="The output directory to populate.")
-    parser.add_argument('--interactive', '-i', action='store_true', help="Run the script in interactive mode.")
 
     args = parser.parse_args()
     args.input = os.path.normpath(args.input)
@@ -42,65 +39,6 @@ def find_oldest_dir(ordered_names: list[str], dirnames: list[str], excluded_dirn
         if name in dirnames and name not in excluded_dirnames:
             return name
     return ''
-
-def sync_oldest(args: argparse.Namespace, month_order: list[str]) -> None:
-    years = sorted(os.listdir(args.input))
-
-    for year in years:
-        input_path = os.path.join(args.input, year)
-        if not os.path.isdir(input_path):
-            print(f"info: skip: ignore non-directory '{input_path}'")
-            continue
-
-        output_path = os.path.join(args.output, year)
-        if not os.path.exists(output_path):
-            print(f"info: {output_path} does not exist, will create")
-            os.mkdir(output_path)
-
-        oldest_dir = find_oldest_dir(month_order, os.listdir(input_path), os.listdir(output_path))
-        input_path = os.path.join(input_path, oldest_dir)
-        output_path = os.path.join(output_path, oldest_dir)
-        input_path_top = input_path
-
-        if os.path.exists(output_path):
-            print("info: will check for sync within directories")
-            for dirname in os.listdir(input_path_top):
-                if not os.path.isdir(os.path.join(input_path, dirname)):
-                    print(f"info: skip: ignore non-directory '{os.path.join(input_path, dirname)}'")
-                    continue
-                input_path = os.path.join(input_path, dirname)
-                output_path = os.path.join(output_path, dirname)
-
-                sync_paths = set(os.listdir(input_path)).difference(os.listdir(output_path))
-                print(f"sync paths: {sync_paths}")
-                # breakpoint()
-                for path in sync_paths:
-                    input_path = os.path.join(input_path, path)
-                    output_path = os.path.join(output_path, path)
-                    if not os.path.isdir(input_path):
-                        print(f"info: skip: ignore non-directory '{input_path}'")
-                        continue
-                    if os.path.exists(output_path):
-                        print(f"error: path exists in ouput but not in input: '{os.path.join(output_path, path)}'")
-                        continue
-
-                    if not os.path.exists(output_path):
-                        print(f"info: will copy '{input_path} -> {input_path}'")
-                        if args.interactive:
-                            choice = input("continue? [y/N]")
-                            if choice != 'y':
-                                print("info: skip: user chose not to copy '{dirname}'")
-                                break
-                        shutil.copytree(input_path, output_path)
-        else:
-            print(f"info: will copy '{input_path}' -> '{output_path}'")
-            if args.interactive:
-                choice = input("continue? [y/N]")
-                if choice != 'y':
-                    print("info: user quit")
-                    break
-            shutil.copytree(input_path, output_path)
-            break
 
 def enumerate_paths(top: str) -> list[str]:
     paths: list[str] = []
