6ed7b8f4d7f7f880c9fa97e5d11489b136b3d4bf
zachvp <vegaperk@gmail.com>
2024-01-09 15:05:54 -0700

    extend script to take options for output format

--- djmgmt/extract_playlist_info.py
+++ djmgmt/extract_playlist_info.py
@@ -10,7 +10,7 @@ Format
 import argparse
 import os
 
-def extract(path, fields) -> list[str]:
+def extract(path: str, fields: list[int]) -> list[str]:
     output = []
 
     with open(path, 'r', encoding='utf-8') as file:
@@ -21,25 +21,48 @@ def extract(path, fields) -> list[str]:
             line = lines[i].split('\t')
             for f in fields:
                 output_line += f"{line[f]}\t"
-            output.append(output_line.strip())
+            output_line = output_line.strip()
+            if len(output_line) > 0:
+                output.append(output_line)
     return output
 
 def parse_args() -> argparse.Namespace:
     parser = argparse.ArgumentParser()
     parser.add_argument('input', type=str, help="The script input path.")
+    parser.add_argument('--number', '-n', action='store_true', help="Include the track number in the output.")
+    parser.add_argument('--title', '-t', action='store_true', help="Include the title in the output.")
+    parser.add_argument('--artist', '-a', action='store_true', help="Include the artist in the output.")
+    parser.add_argument('--genre', '-g', action='store_true', help="Include the genre in the output.")
 
     args = parser.parse_args()
     args.input = os.path.normpath(args.input)
 
+    # collect all existing given args into a dictionary
+    options = {k:v for k, v in vars(args).items() if v}
+    if len(options) < 2:
+        args.number = True
+        args.title = True
+        args.artist = True
+        args.genre = True
+
     return args
 
 def script(args: argparse.Namespace):
-    NUMBER = 0
-    TITLE = 1
-    ARTIST = 2
-    GENRE = 3
+    number = 0
+    title  = 1
+    artist = 2
+    genre  = 3
 
-    fields = [TITLE, ARTIST]
+    fields: list[int] = []
+    if args.number:
+        fields.append(number)
+    if args.title:
+        fields.append(title)
+    if args.artist:
+        fields.append(artist)
+    if args.genre:
+        fields.append(genre)
+
     extracted = extract(args.input, fields)
     print('\n'.join(extracted))
 
