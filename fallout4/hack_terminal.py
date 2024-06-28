7ad23a58e8fc767056eee51aaadc91be5e8ed263
zachvp <vegaperk@gmail.com>
2024-06-28 15:47:06 -0600

    clean up terminal hack script

--- fallout4/hack_terminal.py
+++ fallout4/hack_terminal.py
@@ -1,9 +1,16 @@
-import os
+'''
+Given an input text file of newline-separated words, this script
+assists a user in guessing Fallout terminal hacking puzzle.
+'''
+
 import sys
 
 # helpers
 def compare_similarity(lhs : str, rhs : str) -> int :
-    assert len(lhs) == len(rhs), 'expecting equal string lengths for input: {} | {}'.format(lhs, rhs)
+    '''
+    Given two strings, returns the number of overlapping letters between the two.
+    '''
+    assert len(lhs) == len(rhs), f"expecting equal string lengths for input: [{lhs} | {rhs}]"
 
     similarity = 0
     for i in range(len(lhs)):
@@ -11,54 +18,56 @@ def compare_similarity(lhs : str, rhs : str) -> int :
             similarity += 1
     return similarity
 
-# state
-
-## input
-file_path = sys.argv[1]
-
-## storage
-guesses = []
-
 ## main
+def script(path: str):
+    '''
+    The main script function.
+    '''
 
-# read file lines into guesses
-# trim each guess in guesses
-with open(file_path, 'r') as file:
-    guesses = [guess.rstrip() for guess in file.readlines()]
+    guesses = []
+    # read file lines into guesses
+    # trim each guess in guesses
+    with open(path, 'r', encoding='utf-8') as file:
+        guesses = [guess.rstrip() for guess in file.readlines()]
 
-# validate input
-assert len(guesses) > 1, 'expecting at least 2 guesses'
-for i in range(1, len(guesses)):
-    assert len(guesses[i]) == len(guesses[i-1]), 'all guesses should be equal length'
+    # validate input
+    assert len(guesses) > 1, 'expecting at least 2 guesses'
+    for i in range(1, len(guesses)):
+        assert len(guesses[i]) == len(guesses[i-1]), 'all guesses should be equal length'
 
-while len(guesses) > 0:
-    prompt = 'next guess'
-    print('{}: {}'.format(prompt, guesses[0]))
+    while len(guesses) > 0:
+        prompt = 'next guess'
+        print(f"{prompt}: {guesses[0]}")
 
-    guess = guesses.pop(0)
+        guess = guesses.pop(0)
 
-    # read the result of the guess
-    prompt = 'was the guess correct (y/n)?\n'
-    result = input(prompt).lower()
-    if result == 'y':
-        print('SUCCESS! Exiting...')
-        exit()
-    elif result == 'n':
-        # input
-        prompt = 'guess hint (integer)?\n'
-        hint = int(input(prompt))
-        possibilities = []
+        # read the result of the guess
+        prompt = 'was the guess correct (y/n)?\n'
+        result = input(prompt).lower()
+        if result == 'y':
+            print('SUCCESS!\nExiting...')
+            sys.exit()
+        elif result == 'n':
+            # input
+            prompt = 'guess hint (integer)?\n'
+            hint = int(input(prompt))
+            possibilities = []
 
-        # compare remaining guesses with recent guess + hint
-        for possibility in guesses:
-            similarity = compare_similarity(guess, possibility)
+            # compare remaining guesses with recent guess + hint
+            for possibility in guesses:
+                similarity = compare_similarity(guess, possibility)
 
-            if similarity == hint:
-                possibilities.append(possibility)
+                if similarity == hint:
+                    possibilities.append(possibility)
 
-        guesses = possibilities
-    else:
-        raise Exception('InvalidInput: {}. Expect y/n'.format(result))
+            guesses = possibilities
+        else:
+            raise Exception("InvalidInput: {result}. Expect y/n")
 
 
+if __name__ == '__main__':
+    print('runing hack helper...')
+    ## input
+    file_path = sys.argv[1]
+    script(file_path)
     
