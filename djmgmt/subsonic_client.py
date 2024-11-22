2ecd800b0b6e766fb4c28b8218829b1fd8abf446
zachvp <vegaperk@gmail.com>
2024-11-21 20:43:45 -0700

    fetch API client password from keyring

--- djmgmt/subsonic_client.py
+++ djmgmt/subsonic_client.py
@@ -17,6 +17,8 @@ import hashlib
 import random
 import string
 import json
+import xml.etree.ElementTree as ET
+import keyring
 
 def create_token(password: str, salt: str) -> str:
     return hashlib.md5((password + salt).encode()).hexdigest()
@@ -25,7 +27,8 @@ def create_salt(length):
     return ''.join(random.choices(string.ascii_letters + string.digits, k=length))
 
 # construct query params
-password = ''
+password = keyring.get_password('navidrome_client', 'api_client')
+assert password, f'unable to fetch password'
 salt = create_salt(12)
 base_params = {
     'u': 'api_client',                      # user
@@ -44,10 +47,31 @@ print(f'send request: {url}')
 response = requests.get(url)
 
 # handle the response
-response = {
-    'url': response.url,
-    'status_code': response.status_code,
-    'text': response.text
-}
-print(json.dumps(response, indent=2))
+# dbg = {
+#     'url': response.url,
+#     'status_code': response.status_code,
+#     'text': response.text
+# }
+# print(json.dumps(dbg, indent=2))
+
+response_content = ET.fromstring(response.text).attrib
+if response.status_code == 200:
+    print(f'successful call to {endpoint}')
+    if endpoint == 'ping':
+        status = response_content['status']
+        print(f'status: {status}')
+        
+else:
+    '''
+    0 	A generic error.
+    10 	Required parameter is missing.
+    20 	Incompatible Subsonic REST protocol version. Client must upgrade.
+    30 	Incompatible Subsonic REST protocol version. Server must upgrade.
+    40 	Wrong username or password.
+    41 	Token authentication not supported for LDAP users.
+    50 	User is not authorized for the given operation.
+    60 	The trial period for the Subsonic server is over. Please upgrade to Subsonic Premium. Visit subsonic.org for details.
+    70 	The requested data was not found.
+    '''
+    print(f'error: {response.text}')
 
