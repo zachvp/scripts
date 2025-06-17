34c89308c6d7ec1195b1f314ced35492cdd28b9d
zachvp <vegaperk@gmail.com>
2025-06-16 21:10:10 -0600

    add test for find date context

--- djmgmt/test/test_common.py
+++ djmgmt/test/test_common.py
@@ -12,7 +12,7 @@ sys.path.append(PROJECT_ROOT)
 
 from src import common
 
-class CommonTest(unittest.TestCase):
+class TestConfigureLog(unittest.TestCase):
     @patch('logging.basicConfig')
     @patch('os.path.exists')
     @patch('os.makedirs')
@@ -44,6 +44,35 @@ class CommonTest(unittest.TestCase):
         LOG_PATH = f"{PROJECT_ROOT}/test/logs/test_common.log"
         self.assertEqual(mock_basic_config.call_args.kwargs['filename'], LOG_PATH)
         self.assertEqual(mock_basic_config.call_args.kwargs['level'], logging.INFO)
+        
+class TestFindDateContext(unittest.TestCase):
+    def test_success_basic(self) -> None:
+        path = '/data/tracks-output/2022/04 april/24/1-Gloria_Jones_-_Tainted_Love_(single_version).mp3'
+        actual = common.find_date_context(path)
+        
+        self.assertIsNotNone(actual)
+        self.assertEqual(len(actual), 2)
+        self.assertEqual(actual, ('2022/04 april/24', 3))
+    
+    def test_success_tricky_metadata_subpath(self) -> None:
+        path = '/Users/user/developer/test-private/data/tracks-output/2024/08 august/18/Paolo Mojo/1983/159678_1983_(Eric_Prydz_Remix).aiff'
+        actual = common.find_date_context(path)
+        
+        self.assertIsNotNone(actual)
+        self.assertEqual(len(actual), 2)
+        self.assertEqual(actual, ('2024/08 august/18', 7))
+    
+    def test_success_invalid_month_name(self):
+        path = '/mock/input/2025/08 aug/22/artist/album/track.mp3'
+        actual = common.find_date_context(path)
+        
+        self.assertIsNone(actual)
+        
+    def test_success_invalid_month_index(self):
+        path = '/mock/input/2025/01 august/22/artist/album/track.mp3'
+        actual = common.find_date_context(path)
+        
+        self.assertIsNone(actual)
 
 if __name__ == '__main__':
     unittest.main()
