03f8b638e562fffff14757201ce1ba7badf56601
zachvp <vegaperk@gmail.com>
2025-06-27 23:17:53 -0600

    rm test context file

--- djmgmt/test/context.py
+++
@@ -1,7 +0,0 @@
-'''Provides import context for the target module.'''
-import os
-import sys
-_PROJECT_DIR = os.path.join(os.path.dirname(__file__), os.path.pardir)
-sys.path.insert(0, os.path.abspath(_PROJECT_DIR))
-
-import src
